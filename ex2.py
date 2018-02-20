24ba6d23e9bee228779eb01c5d4005916a6d3731
Moiseser <moiseser@uci.edu>
2018-02-19 17:59:37 -0800

    added python tutorials

---
+++ ex2.py
@@ -0,0 +1,7 @@
+#Text after hashtags are comments and not read by python
+#These can be used to write details about your code or 'tag out' parts of code
+print 'I could have code like this' # this will be ignored
+
+#print ' this will not print'
+
+print 'This will run'
